20d7274d3455b9d05815527926cf3dc109e93b43
Mustafa Kazı <89959427+TheMusoftware@users.noreply.github.com>
2023-02-15 17:30:39 +0300

    -

--- PDev GradeAverage/src/Main.java
+++ PDev GradeAverage/src/Main.java
@@ -4,18 +4,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         double average=0;
+        double placeHolder=0;
         System.out.print("Grade of Turkish Language: ");
-        average+=scan.nextInt();
+        placeHolder=scan.nextInt();
+        if(placeHolder>55)average+=placeHolder;
+        placeHolder=0;
         System.out.print("Grade of Math : ");
-        average+=scan.nextInt();
+        placeHolder=scan.nextInt();
+        if(placeHolder>55)average+=placeHolder;
+        placeHolder=0;
         System.out.print("Grade of Physics: ");
-        average+=scan.nextInt();
+        placeHolder = scan.nextInt();
+        if(placeHolder>55)average+=placeHolder;
+        placeHolder=0;
         System.out.print("Grade of Chemistry: ");
-        average+=scan.nextInt();
-        System.out.print("Grade of History: ");
-        average+=scan.nextInt();
+        placeHolder=scan.nextInt();
+        if(placeHolder>55)average+=placeHolder;
+        placeHolder=0;
         System.out.print("Grade of Music: ");
-        average+=scan.nextInt();
+        placeHolder=scan.nextInt();
+        if(placeHolder>55)average+=placeHolder;
+        placeHolder=0;
         String result=(average/5)>60 ? "You have completed this term successfully." :"You have failed this term.";
         System.out.println("Average: "+(average/5)+"\n"+result);
 
