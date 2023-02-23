7727191b9bff73a6f2dd1dba2daf5209ec8578a9
Mustafa Kazı <89959427+TheMusoftware@users.noreply.github.com>
2023-02-23 15:40:43 +0300

    New Salary Calculator
    Salary

---
+++ PDev Salary/src/Main.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Mustafa Kazı",4000,60,2002);
+        employee.showInfo();
+
+    }
+}
